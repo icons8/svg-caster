706bd71016d232a3729b466054bc51171d9b5db6
Viacheslav Bereza <betula.icons8@gmail.com>
2015-05-21 17:19:57 +0300

    Add SVGO filter

--- bin/svg-caster.js
+++ bin/svg-caster.js
@@ -9,22 +9,23 @@ var
   argv = yargs
     .usage('Usage: $0 [options]')
 
-    .describe('svg-font', 'Path to svg font file')
-    .describe('svg-font-css', 'Path to svg font css file')
-    .describe('svg-font-css-prefix', 'Prefix for icon names in font css file')
-    .describe('svg', 'Path/Pattern to svg file/files')
-    .describe('svg-set', 'Path/Pattern to svg set file/files')
-    .describe('out-svg', 'Path to output svg files folder')
-    .describe('out-svg-set', 'Path to output svg set file')
-    .describe('shorty', 'Shortify output svg and svg sets')
-    .describe('pretty', 'Prettify output svg and svg sets')
+    .describe('svg-font', 'Path to SVG font file')
+    .describe('svg-font-css', 'Path to SVG font CSS file')
+    .describe('svg-font-css-prefix', 'Prefix for icon names in font CSS file')
+    .describe('svg', 'Path/Pattern to SVG file/files')
+    .describe('svg-set', 'Path/Pattern to SVG set file/files')
+    .describe('out-svg', 'Path to output SVG files folder')
+    .describe('out-svg-set', 'Path to output SVG set file')
+    .describe('svgo', 'Optimize SVG with SVGO')
+    .describe('shorty', 'Shortify output SVG and SVG sets')
+    .describe('pretty', 'Prettify output SVG and SVG sets')
 
     .help('h')
     .alias('h', 'help')
 
-    .example('$0 --svg-font ./font.svg --svg-font-css ./font.css --out-svg ./svg/', 'Convert svg font to svg files')
-    .example('$0 --svg-font ./font.svg --svg "./svg/*.svg" --out-svg-set ./svg-sprite.svg', 'Convert svg font to svg set file')
-    .example('$0 --svg-set "./one/*.svg" --svg-set "./two/*.svg" --out-svg-set ./out.svg', 'Convert multiple svg set files to one')
+    .example('$0 --svg-font ./font.svg --svg-font-css ./font.css --out-svg ./svg/', 'Convert SVG font to SVG files')
+    .example('$0 --svg-font ./font.svg --svg "./svg/*.svg" --out-svg-set ./svg-sprite.svg', 'Convert SVG font to SVG set file')
+    .example('$0 --svg-set "./one/*.svg" --svg-set "./two/*.svg" --out-svg-set ./out.svg', 'Convert multiple SVG set files to one')
 
     .epilog('svg-caster (https://github.com/icons8/svg-caster)')
     .argv,
